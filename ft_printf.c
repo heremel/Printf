716cdf70c6b4bfdb5b53ca756c3686b17ee72493
heremel <melissaferreira2510@gmail.com>
2024-11-24 18:00:47 +0100

    hexa base done

--- ft_printf.c
+++ ft_printf.c
@@ -12,11 +12,37 @@
 
 #include "libftprintf.h"
 
-int	ft_hexa_base()
+void	ft_hexa_base(unsigned int n)
 {
-	int hexa;
+	char *hexa;
 
 	hexa = "123456789abcdef";
+	if (n == 0)
+	{
+		ft_putchar('0');
+		return;
+	}
+	else if (n >= 16)
+	{
+		ft_hexa_base(n / 16);
+	}
+	ft_putchar(hexa[n % 16]);
+}
+void	ft_hexa_base_uppercase(unsigned int n)
+{
+	char *hexa;
+
+	hexa = "0123456789ABCDEF";
+	if (n == 0)
+	{
+		ft_putchar('0');
+		return;
+	}
+	else if (n >= 16)
+	{
+		ft_hexa_base_uppercase(n / 16);
+	}
+	ft_putchar(hexa[n % 16]);
 }
 int	ft_format(char c, va_list list)
 {
@@ -33,9 +59,9 @@ int	ft_format(char c, va_list list)
 	else if (c == '%')
 		ft_putchar('%');
 	else if (c == 'x')
-		//hexa lowerrcase
+		ft_hexa_base(va_arg(list, unsigned int));
 	else if (c == 'X')
-		//hexa uppercase
+		ft_hexa_base_uppercase(va_arg(list, unsigned int));
 }
 int ft_printf(char *str, ...)
 {
